7cf5b11627c3037645734d4c621949e75a0c8f3c
Stanislav Panchenko <stanip@hotmail.de>
2022-01-03 10:39:56 +0100

    fix: basename and homepage link

--- site/index.tsx
+++ site/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import { basename, production } from "../env";
-
+//
 ReactDOM.render(
   <BrowserRouter basename={production ? basename : ""}>
     <App />
