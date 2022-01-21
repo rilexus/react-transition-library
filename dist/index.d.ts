9afbbc90ae85cad54f9ba49dc4985e6bbe5876a3
rilexus <stanip@hotmail.de>
2022-01-21 10:26:04 +0100

    feat(utils): added mergeRef

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,5 @@ export * from "./transitions";
 export * from "./ease";
 export * from "./hooks";
 export * from "./utils/createEventTransition";
+export * from "./utils/mergeRefs";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,4 @@ export * from "./transitions";
 export * from "./ease";
 export * from "./hooks";
 export * from "./utils/createEventTransition";
+export * from "./utils/mergeRefs";
